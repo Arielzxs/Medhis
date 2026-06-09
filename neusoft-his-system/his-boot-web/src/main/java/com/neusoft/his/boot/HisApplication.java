3d8a8e1bc488c0b3ea80866343fbaeab39e26e7b
Ariel <13345068065@163.com>
2026-06-09 15:50:32 +0800

    0609

--- neusoft-his-system/his-boot-web/src/main/java/com/neusoft/his/boot/HisApplication.java
+++ neusoft-his-system/his-boot-web/src/main/java/com/neusoft/his/boot/HisApplication.java
@@ -1,10 +1,12 @@
 package com.neusoft.his.boot;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication(scanBasePackages = "com.neusoft.his")
+// 新增这一行，指定 Mapper 接口所在的包路径
+@MapperScan("com.neusoft.his.dal.mapper")
 public class HisApplication {
     public static void main(String[] args) {
         SpringApplication.run(HisApplication.class, args);
